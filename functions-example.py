c209e62daab5cc7c60d1313c8ae9ffa9b6429d69
Chaithanya <chaithanya.bangaru@github.com>
2019-01-30 10:03:51 -0600

    variable args function, keyword args function example

--- functions-example.py
+++ functions-example.py
@@ -7,7 +7,7 @@ def printNames():
     student_list = get_students()
     # print each student
     for student in students:
-        print(student)
+        print(student,"\n")
 
 def get_students():
     return students
@@ -20,10 +20,24 @@ def add_student(fname,mi,lname,std_id):
 
 def var_args(name, *args):
     print(name)
-    #print(args)
+    print(args,"\n")
+
+def keyword_args_func(firstname, **kwargs):
+    print(firstname,"\n")
+    print(kwargs["lastname"],"\n")
+    print(kwargs["lastname"],"\n")
+    print(kwargs["lastname"],"\n")
+
+def keyword_args_func2(firstname, **kwargs):
+        print(firstname)
+        print(kwargs["lastname"], "\n")
+
+printNames()
 
 add_student("Raja","C","Bangaru",326)
 var_args("Ivaan", "Vijay","Bangaru", 6 ,"years old.")
-printNames()
+keyword_args_func(firstname="Ivaan", mi="Vijay",lastname="Bangaru", age=6 ,comment="years old.")
+
+keyword_args_func2(firstname="Ivaan", mi="Vijay",lastname="Bangaru", age=6 ,comment="years old.")
 
 
